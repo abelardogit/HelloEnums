cdbf2639c04ad80902c357402db5dde698717611
OscarRequena <oscarjair_7@hotmail.com>
2025-04-04 18:49:08 +0200

    CORRECCION DEL NUM

--- src/main/java/HelloEnumsApp.java
+++ src/main/java/HelloEnumsApp.java
@@ -1,6 +1,6 @@
 public class HelloEnumsApp {
     public static void main(String[] args) {
-        Ordenacion ordenacion = obtenerOrdenCadena1("Albania", "Alba");
+        Ordenacion ordenacion = obtenerOrdenCadena1("Albania", "AlbaniA");
         System.out.println(ordenacion);
     }
 
@@ -28,7 +28,15 @@ public class HelloEnumsApp {
 
         int numMaxChars = Math.min(numCarsCad1, numCarsCad2);
 
+        //FUNCIONA !!! :)
+        if (numCarsCad1 > numCarsCad2)
+            return isGreater;
+
+        if (numCarsCad1 < numCarsCad2)
+            return !isGreater;
+
         while(posCurrentChar < numMaxChars && isGreater) {
+
             char currentChar1 = carsCad1[posCurrentChar];
             char currentChar2 = carsCad2[posCurrentChar];
             isGreater = currentChar1 > currentChar2;
